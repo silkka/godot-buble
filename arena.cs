586221200788d3aac11cb8700507694776c2efaf
Silkka <silkka>
2023-12-30 16:38:11 +0200

    Drawing works

--- arena.cs
+++ arena.cs
@@ -1,23 +1,43 @@
-using System;
 using System.Collections.Generic;
 using Godot;
+using HexGrid;
 
-public partial class arena : Node2D
+public partial class Arena : Node2D
 {
     [Export]
     PackedScene Ball_Scene;
 
     private List<Ball> Grid;
+    private Vector2[] points;
+    private Color[] colors;
 
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
+    public override void _Ready()
+    {
+        (points, colors) = Create();
+    }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta) { }
 
-    public void Create()
+    public override void _Draw()
     {
-        Grid = new List<Ball>();
-        Console.WriteLine("Arena created", Grid);
+        DrawPolygon(points, colors);
+    }
+
+    public static (Vector2[], Color[]) Create()
+    {
+        Layout layout =
+            new(
+                orientation: HexGrid.Orientation.POINTY,
+                size: new Vector2(32, 32),
+                origin: new Vector2(100, 100)
+            );
+
+        Vector2[] points = Layout.PolygonCorners(layout, new Hex(0, 0));
+        Color color = new(0.5f, 0.5f, 0.5f, 1.0f);
+        Color[] colors = new Color[6] { color, color, color, color, color, color };
+
+        return (points, colors);
     }
 }
--- utils/hex-grid/Layout.cs
+++ utils/hex-grid/Layout.cs
@@ -35,5 +35,27 @@ namespace HexGrid
             float r = M.B2 * pt.X + M.B3 * pt.Y;
             return new FractionalHex(q, r);
         }
+
+        public static Vector2 HexCornerOffset(Layout layout, int corner)
+        {
+            Orientation M = layout.Orientation;
+            float angle = 2.0f * (float)Math.PI * (M.StartAngle + corner) / 6.0f;
+            return new Vector2(
+                layout.Size.X * (float)Math.Cos(angle),
+                layout.Size.Y * (float)Math.Sin(angle)
+            );
+        }
+
+        public static Vector2[] PolygonCorners(Layout layout, Hex h)
+        {
+            Vector2[] corners = new Vector2[6];
+            Vector2 center = HexToPixel(layout, h);
+            for (int i = 0; i < 6; i++)
+            {
+                Vector2 offset = HexCornerOffset(layout, i);
+                corners[i] = new Vector2(center.X + offset.X, center.Y + offset.Y);
+            }
+            return corners;
+        }
     }
 }
