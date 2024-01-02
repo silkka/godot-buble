bf684e154b5a765b6dd2ebeace188dbc59c73268
Silkka <silkka>
2024-01-02 15:51:23 +0200

    Unused

--- Ball.cs
+++ Ball.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Threading;
 using Godot;
 
 public partial class Ball : Area2D
--- game.cs
+++ game.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
 using Godot;
-using HexGrid;
 
 public partial class Game : Node2D
 {
